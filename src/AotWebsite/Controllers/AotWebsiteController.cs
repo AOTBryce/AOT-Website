ae0c475ed1f1efcd8574a63b35707c7c35cddcd3
Bryce <AOTBryce@gmail.com>
2016-11-23 20:41:24 -0500

    added AotWebsite detail view

--- src/AotWebsite/Controllers/AotWebsiteController.cs
+++ src/AotWebsite/Controllers/AotWebsiteController.cs
@@ -6,7 +6,18 @@ using System.Web.Mvc;
 
 namespace AotWebsite.Controllers
 {
+    /*
+    Every public method in a controller is an action method
+         */
+
     public class AotWebsiteController : Controller
     {
+
+        public ActionResult Detail()
+        {
+            return View();
+
+        }
+
     }
 }
