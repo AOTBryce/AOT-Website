c2b942c073be915a5c34fb95f070faad0723f4d3
Bryce <AOTBryce@gmail.com>
2016-11-25 12:09:41 -0500

    Updated Project model

--- src/AotWebsite/Controllers/AotWebsiteController.cs
+++ src/AotWebsite/Controllers/AotWebsiteController.cs
@@ -19,12 +19,17 @@ namespace AotWebsite.Controllers
             ViewBag.CompanyName = "AOT";
             ViewBag.StartYear = 2016;
             ViewBag.Description = "<p><u>Nothing in this world thats worth having comes easy</u></p>";
-            ViewBag.Projects = new string[]
+            ViewBag.Developers = new string[]
             {
-                "Project 1: 2016",
-                "Project 2: 2017",
-                "Project 3: 2017",
-                "Project 4: 2018"
+                "HTML: HTML5Up",
+                "ASP.Net Developer: AOTBryce",
+                "Graphics: TBD",
+                "Hosting: AWS"
+                
+                //"Project 1: 2016",
+                //"Project 2: 2017",
+                //"Project 3: 2017",
+                //"Project 4: 2018"
             };
             //below value to be swapped in for projects in order to demonstrate usefulness of if statement
             var projects2 = new string[] { };
---
+++ src/AotWebsite/Models/Project.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AotWebsite.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string ProjectTitle { get; set; }
+        public int YearReleased { get; set; }
+        public string DescriptionHtml { get; set; }
+        public Developer[] Developers { get; set; }
+        public bool Released { get; set; }
+
+        //note that this value is readonly (can not be set because it has no setter) can only be gotten
+        public string DisplayText
+        {
+            get
+            {
+                return ProjectTitle + " :" + YearReleased;
+            }
+        }
+
+            //file names for images should follow a convention that allows them to be dynamically called based on the information in the model.
+            //e.g. for the ComicBook example the patter follows <series-title-issuenumber.jpg>
+            //By saving image files (or other source files) as such it makes it very easy to call them with a dynamic variable
+            //e.g. Each model can have a string named <imageName> that concatonates the information and then can be used to
+            //call the desired image
+
+            //obviously this method will not work until I have each project set up with a corresponding jpg.
+            public string CoverImageFileName
+        {
+            get
+            {
+                return ProjectTitle.Replace(" ", "-").ToLower() + "-" + YearReleased + ".jpg";
+            }
+        }
+    }
+}
